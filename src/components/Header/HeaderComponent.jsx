64373afa6dd0cf3e65ef49ebbc98a8a3644a5d6a
Icaro Silva Rodrigues <icaro2rodrigues@gmail.com>
2024-03-11 01:35:26 -0300

    feat:add menu burger

--- src/components/Header/HeaderComponent.jsx
+++ src/components/Header/HeaderComponent.jsx
@@ -1,36 +1,64 @@
-import './header.style.css'
+import React, { useState } from 'react';
+import './header.style.css';
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoMdExit } from "react-icons/io";
 
-function HeaderComponent
-({imagens}) {
-    return ( 
+function HeaderComponent({ imagens }) {
+    // Define o estado para controlar a visibilidade do menu mobile
+    const [mobileMenuVisible, setMobileMenuVisible] = useState(false);
+    // Define o estado para controlar qual ícone de menu mostrar
+    const [menuIcon, setMenuIcon] = useState(<RxHamburgerMenu />);
+
+    // Função para alternar a visibilidade do menu mobile e mudar o ícone
+    const toggleMobileMenu = () => {
+        setMobileMenuVisible(!mobileMenuVisible);
+        // Se o menu mobile está visível, mostra o ícone de saída, caso contrário mostra o ícone do menu hamburger
+        setMenuIcon(mobileMenuVisible ? <RxHamburgerMenu /> : <IoMdExit />);
+    };
+
+    return (
         <header>
             <div className="container-header">
-                <div className="flex">
-                <a href="#">
-            <img src={imagens[0]} alt="" />
-                </a>
+                <div className="header-content">
+                    <a href="#">
+                        <img src={imagens[0]} alt="" />
+                    </a>
 
-            <nav>
-                
-                <ul>
-                  <a href="#about-me"><li >Sobre mim</li></a>  
-                  <a href="#container-tec"><li >Tecnologias</li></a>  
-                  <a href="#container-site"><li >Projetos</li></a>  
-                  <a href="#contact"><li >Contato</li></a>  
-                    
-                </ul>
-            </nav>
-            <div className="btn-github">
-                
-                <a href="https://github.com/PZicaro"><button><img src={imagens[1]} alt="" /> GitHub</button></a>
-            </div>
+                    <div className="flex">
+                        <nav>
+                            <ul>
+                                <a href="#about-me"><li >Sobre mim</li></a>
+                                <a href="#container-tec"><li >Tecnologias</li></a>
+                                <a href="#container-site"><li >Projetos</li></a>
+                                <a href="#contact"><li >Contato</li></a>
+                            </ul>
+                        </nav>
+                        <div className="btn-github">
+                            <a href="https://github.com/PZicaro"><button><img src={imagens[1]} alt="" /> GitHub</button></a>
+                        </div>
+                    </div>
 
+                    <div className="menu-hamburger" onClick={toggleMobileMenu}>
+                        {menuIcon}
+                    </div>
+
+                </div>
+
+                <div className={`mobile-menu ${mobileMenuVisible ? 'show' : ''}`}>
+                    <nav>
+                        <ul>
+                            <a href="#about-me"><li >Sobre mim</li></a>
+                            <a href="#container-tec"><li >Tecnologias</li></a>
+                            <a href="#container-site"><li >Projetos</li></a>
+                            <a href="#contact"><li >Contato</li></a>
+                            <a href="https://github.com/PZicaro"><li >GitHub</li></a>
+                        </ul>
+                    </nav>
                 </div>
 
             </div>
         </header>
-     );
+    );
 }
 
-export default HeaderComponent
-;
+export default HeaderComponent;
